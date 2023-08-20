326cfe694e6a26cbe7580437938fcbb811b73489
Michał Mnich <m.mnich@proton.me>
2023-08-20 02:50:20 +0200

    change: hyprland and waybar multi monitor setup

--- .config/hypr/toggle_dpms.sh
+++ .config/hypr/toggle_dpms.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
-toggle=$(hyprctl monitors | grep dpmsStatus | cut -d " " -f 2)
+toggle=$(hyprctl monitors | grep -m 1 dpmsStatus | cut -d " " -f 2)
 if [[ $toggle == "0" ]]; then
-  hyprctl dispatch dpms on
+  hyprctl dispatch dpms on eDP-1
 elif [[ $toggle == "1" ]]; then
-  hyprctl dispatch dpms off
+  hyprctl dispatch dpms off eDP-1
 fi
