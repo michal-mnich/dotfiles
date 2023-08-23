bd236ce59cf8383a16076d4a9855c7d3bda7e6f0
Michał Mnich <m.mnich@proton.me>
2023-08-24 01:20:51 +0200

    fix: cfg aliases

--- .config/aliases.zsh
+++ .config/aliases.zsh
@@ -6,7 +6,7 @@ alias python="ipython"
 alias rust-book="rustup docs --book"
 
 # Dotflies bare git repo
-alias cfg="git --git-dir=$HOME/.mycfg/ --work-tree=$HOME"
+alias cfg="git --git-dir=$HOME/.mycfg --work-tree=$HOME"
 
 alias cfgc="cfg commit --verbose"
 alias "cfgc!"="cfgc --amend"
@@ -20,12 +20,12 @@ alias cfgst="cfg status"
 alias cfgp="cfg push"
 
 # Configuration files
-alias aliascfg="nvim $HOME/.config/aliases.zsh"
-alias hyprcfg="cd $HOME/.config/hypr"
-alias swaycfg="cd $HOME/.config/sway"
-alias waybarcfg="cd $HOME/.config/waybar"
-alias nvimcfg="cd $HOME/.config/nvim/lua/custom"
-alias kittycfg="cd $HOME/.config/kitty"
+alias zshcfg="cd && nvim ~/.zshrc ~/.zprofile ~/.zshenv ~/.config/aliases.zsh"
+alias hyprcfg="cd ~/.config/hypr && nvim hyprland.conf binds.conf"
+alias swaycfg="cd ~/.config/sway && nvim config"
+alias waybarcfg="cd ~/.config/waybar && nvim config style.css"
+alias nvimcfg="cd ~/.config/nvim/lua/custom && nvim init.lua"
+alias kittycfg="cd ~/.config/kitty && nvim kitty.conf"
 
 # System maintenance
 alias tlupgrade="sudo tlmgr update --all"
