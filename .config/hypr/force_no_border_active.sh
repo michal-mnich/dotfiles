8f195fd34cdc515953f06d8935a9bec65fae56b3
Michał Mnich <m.mnich@proton.me>
2023-08-23 02:33:48 +0200

    fix: match the window by address, not pid

--- .config/hypr/force_no_border_active.sh
+++ .config/hypr/force_no_border_active.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-activePID=$(hyprctl activewindow | grep pid | cut -d " " -f 2)
-hyprctl setprop pid:$activePID forcenoborder $1 lock
+active_address=$(hyprctl activewindow | grep -m 1 Window | cut -d " " -f 2)
+hyprctl setprop address:0x$active_address forcenoborder $1 lock
